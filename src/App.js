8a735b721bbabdf5643047a778028087af02792e
Danmark <danmark.quinones@media-meter.com>
2021-02-11 22:30:09 +0800

    üpdaet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import Main from './components/Main'
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import 'antd/dist/antd.css';
 import {ThemeContextProvider} from './components/context/themeContext'
 import {isMobile} from 'react-device-detect';
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
-        <Router basename={"/midorenji-v3"}>
+        <Router basename={"/portfolio"}>
           {isMobile?
             <h2>Not AVAILABLE</h2>
             :<Main/>
